75f324233f3d53ae8787a923b24a860133164908
gaofengJ <760613910@qq.com>
2022-08-25 00:45:41 +0800

    Feat：update

---
+++ src/20.is-valid.js
@@ -0,0 +1,28 @@
+/**
+ * @description 有效的括号
+ * @field 栈
+ * @description 时间复杂度：O(n)，其中n是字符串的长度
+ * @description 空间复杂度：O(n + ∣Σ∣)。栈中的字符数量为 O(n)，而哈希表使用的空间为 O(∣Σ∣)，相加即可得到总空间复杂度。
+ * @param {*string} s
+ * @returns boolean
+ */
+const isValid = (s) => {
+  const len = s.length;
+  if (len % 2) return false;
+  const parenthesesMap = {
+    '(': ')',
+    '{': '}',
+    '[': ']',
+  };
+  const stack = [];
+  for (let i = 0; i < len; i++) {
+    if (['(', '{', '['].includes(s[i])) {
+      stack.push(s[i]);
+    }
+    if ([')', '}', ']'].includes(s[i])) {
+      const tempStr = stack.pop();
+      if (parenthesesMap[tempStr] !== s[i]) return false;
+    }
+  }
+  return !stack.length;
+};
---
+++ src/21.merge-two-lists.js
@@ -0,0 +1,29 @@
+function ListNode(val, next) {
+  this.val = (val === undefined ? 0 : val);
+  this.next = (next === undefined ? null : next);
+}
+
+/**
+ * @description 合并两个有序链表
+ * @description 时间复杂度：O(n+m)。其中 n 和 m 分别为两个链表的长度。
+ * @description 空间复杂度：O(1)。只需要常数的空间存放若干变量。
+ * @param {*ListNode} list1
+ * @param {*ListNode*} list2
+ * @returns ListNode
+ */
+const mergeTwoLists = (list1, list2) => {
+  const prev = new ListNode(0);
+  let cur = prev;
+  while (list1 && list2) {
+    if (list1.val > list2.val) {
+      cur.next = list2;
+      list2 = list2.next;
+    } else {
+      cur.next = list1;
+      list1 = list1.next;
+    }
+    cur = cur.next;
+  }
+  cur.next = list1 || list2;
+  return prev.next;
+};
