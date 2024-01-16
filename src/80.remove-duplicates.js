046ab3dccc7171a8a2c0a6941e75b47f398c5e91
gaofengJ <760613910@qq.com>
2024-01-16 22:58:29 +0800

    feat: add 80.remove-duplicates

---
+++ src/80.remove-duplicates.js
@@ -0,0 +1,14 @@
+const removeDuplicates = (nums) => {
+  const n = nums.length;
+  if (n < 2) return n;
+  let left = 2;
+  let right = 2;
+  while (right < n) {
+    if (nums[left - 2] !== nums[right]) {
+      nums[left] = nums[right];
+      left++;
+    }
+    right++;
+  }
+  return left;
+};
