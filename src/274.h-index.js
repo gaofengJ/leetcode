05b82471c84cea901067860eb460c97917b702ea
gaofengJ <760613910@qq.com>
2024-04-14 18:15:37 +0800

    feat: update 274

--- src/274.h-index.js
+++ src/274.h-index.js
@@ -1,16 +1,9 @@
 const hIndex = (citations) => {
   let h = 0;
-  for (let i = 0; i < citations.length; i++) {
-    const outer = [citations[i]];
-    if (outer <= h) continue;
-    let count = 0;
-    for (let j = 0; j < citations.length; j++) {
-      if (citations[j] >= outer) {
-        count++;
-      }
-    }
-    if (count > h) {
-      h = Math.min(outer, count);
+  const sortedCitations = citations.sort((a, b) => b - a);
+  for (let i = 0; i < sortedCitations.length; i++) {
+    if (i + 1 <= sortedCitations[i]) {
+      h += 1;
     }
   }
   return h;
