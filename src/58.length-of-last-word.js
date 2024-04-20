cdab25ba28ccdd620d94a86c40a1b3a01d9a071c
木风 <>
2024-04-20 22:15:06 +0800

    feat: update 58

--- src/58.length-of-last-word.js
+++ src/58.length-of-last-word.js
@@ -1,20 +1,4 @@
-// const lengthOfLastWord = (s) => {
-//   const arr = s.trim().split(' ');
-//   return arr[arr.length - 1].length;
-// };
-
 const lengthOfLastWord = (s) => {
-  let num = 0;
-  let isStart = false;
-  for (let i = s.length - 1; i >= 0; i--) {
-    console.log(s[i], !s[i]);
-    if (s[i] === ' ' && isStart) return num;
-    if (s[i] !== ' ') {
-      isStart = true;
-      num++;
-    }
-  }
-  return num;
+  const arr = s.trim().split(' ');
+  return arr[arr.length - 1].length;
 };
-
-lengthOfLastWord('Hello World');
