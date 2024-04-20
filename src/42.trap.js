bcc0423e22020a2feefccde69d1d6470905ef607
木风 <>
2024-04-20 15:57:00 +0800

    feat: add 42

--- src/42.trap.js
+++ src/42.trap.js
@@ -25,3 +25,27 @@
 // };
 
 // trap([2, 0, 2]);
+
+/**
+ * 按列求
+ * 题解：https://leetcode.cn/problems/trapping-rain-water/solutions/9112/xiang-xi-tong-su-de-si-lu-fen-xi-duo-jie-fa-by-w-8/?envType=study-plan-v2&envId=top-interview-150
+ */
+const trap = (height) => {
+  const leftMax = [];
+  const rightMax = [];
+  let sum = 0;
+  for (let i = 1; i < height.length - 1; i++) {
+    leftMax[i] = Math.max(leftMax[i - 1] || 0, height[i - 1]);
+  }
+  for (let i = height.length - 2; i > 0; i--) {
+    rightMax[i] = Math.max(rightMax[i + 1] || 0, height[i + 1]);
+  }
+  console.log(leftMax, rightMax);
+  for (let i = 1; i < height.length - 1; i++) {
+    const tempMin = Math.min(leftMax[i], rightMax[i]);
+    if (tempMin > height[i]) {
+      sum += tempMin - height[i];
+    }
+  }
+  return sum;
+};
