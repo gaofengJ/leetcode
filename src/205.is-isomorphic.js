1421ad2686ba0495f20f7611267a0bb2445a62e5
gaofengJ <760613910@qq.com>
2024-04-28 19:20:02 +0800

    feat: add 205

---
+++ src/205.is-isomorphic.js
@@ -0,0 +1,13 @@
+const isIsomorphic = (s, t) => {
+  const mapS = new Map();
+  const mapT = new Map();
+  for (let i = 0; i < s.length; i++) {
+    if (!mapS.has(s[i])) {
+      mapS.set(s[i], t[i]);
+    } else if (mapS.get(s[i]) !== t[i]) return false;
+    if (!mapT.has(t[i])) {
+      mapT.set(t[i], s[i]);
+    } else if (mapT.get(t[i]) !== s[i]) return false;
+  }
+  return true;
+};
