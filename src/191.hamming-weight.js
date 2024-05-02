1a1545aa97e374267b659647aff2c7de27b38c71
gaofengJ <760613910@qq.com>
2024-05-02 23:33:52 +0800

    feat: update 191

--- src/191.hamming-weight.js
+++ src/191.hamming-weight.js
@@ -5,13 +5,22 @@
  * @param {number} n
  * @returns
  */
+// const hammingWeight = (n) => {
+//   let ret = 0;
+//   for (let i = 0; i < 32; i++) {
+//     // eslint-disable-next-line no-bitwise
+//     if ((n & (1 << i)) !== 0) {
+//       ret++;
+//     }
+//   }
+//   return ret;
+// };
+
 const hammingWeight = (n) => {
+  const arr = n.toString(2).split('');
   let ret = 0;
-  for (let i = 0; i < 32; i++) {
-    // eslint-disable-next-line no-bitwise
-    if ((n & (1 << i)) !== 0) {
-      ret++;
-    }
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === '1') ret++;
   }
   return ret;
 };
