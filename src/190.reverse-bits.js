4b005f1e1f8954ad4a7550251604a82ea46367b5
gaofengJ <760613910@qq.com>
2024-05-02 23:22:51 +0800

    feat: update 190

--- src/190.reverse-bits.js
+++ src/190.reverse-bits.js
@@ -6,11 +6,27 @@
  * @param {number} n
  * @returns
  */
+// const reverseBits = (n) => {
+//   let rev = 0;
+//   for (let i = 0; i < 32 && n > 0; ++i) {
+//     rev |= (n & 1) << (31 - i);
+//     n >>>= 1;
+//   }
+//   return rev >>> 0;
+// };
+
 const reverseBits = (n) => {
-  let rev = 0;
-  for (let i = 0; i < 32 && n > 0; ++i) {
-    rev |= (n & 1) << (31 - i);
-    n >>>= 1;
+  let arr = `${n.toString(2)}`.split('');
+  arr = [...new Array(32 - arr.length).fill('0'), ...arr];
+  const len = arr.length;
+  let left = 0;
+  let right = len - 1;
+  while (left < right) {
+    const temp = arr[left];
+    arr[left] = arr[right];
+    arr[right] = temp;
+    left++;
+    right--;
   }
-  return rev >>> 0;
+  return parseInt(arr.join(''), 2);
 };
